d707dbd4411216e83637491f207ffd12dd8b04b6
hyunjiiikim <hjk385900@gmail.com>
2025-06-12 15:48:08 +0900

    github finder

---
+++ study/stack.js
@@ -0,0 +1,52 @@
+//stack은 LIFO
+//브라우저 뒤로 가기, 함수 호출 등에 이용
+//배열의 push, pop을 이용해 구현
+
+class Stack {
+    constructor() { //생성자 함수
+        this.item = [];
+    }
+
+    push(element) {
+        this.item.push(element);
+        console.log('push : ' + element);
+    }
+
+    pop() {
+        if (this.isEmpty()) {
+            console.log('Empty!');
+            return;
+        }
+        console.log('pop : ' + this.item.pop());
+    }
+
+    isEmpty() {
+        return this.item.length === 0;
+    }
+
+    peek() {
+        console.log('top element :' + this.item[this.item.length - 1]); //배열 인덱스 0부터 시작이라 -1, this.item.length 자체는 값이 아닌 인덱스라 this.item[]으로 묶어줘야 함
+    }
+
+    clear() {
+        this.item = [];
+    }
+
+    print() {
+        console.log('stack : ' + this.item.join(', '));
+    }
+}
+
+const stack = new Stack();
+stack.push(1);
+stack.push(2);
+stack.push(3);
+stack.print();
+stack.pop();
+stack.peek();
+stack.pop();
+stack.peek();
+stack.pop();
+stack.peek();
+stack.pop();
+stack.print();
---
+++ week_4/github.js
@@ -0,0 +1,26 @@
+class Github {
+  constructor() {
+    this.client_id = 'Ov23liUUP8kz8WDj5taA';
+    this.client_secret = '4af0fc02282550edffa691fe9f10ecdcec732913';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    };
+  }
+}
